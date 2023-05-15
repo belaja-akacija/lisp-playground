f299cf0eca34933d5331dd8a68401a5b36fafd91
belaja-akacija <BA@pm.me>
2023-05-14 17:15:15 -0700

    mucking about with the scroll function

--- scroll/scroll.lisp
+++ scroll/scroll.lisp
@@ -1,4 +1,10 @@
+;#!/home/belajaakacija/.local/bin/sbcl --script
 ;;;; Take a string from stdin and make it scroll across the screen
+
+
+(load "~/quicklisp/setup.lisp")
+(ql:quickload "uiop" :silent t)
+;(ql:quickload :with-user-abort :silent t)
 
 (defun string->list (str)
   (let ((list-string '()))
@@ -18,6 +24,7 @@
 ;; nreverse it again
 
 (defun scroll (str)
+  "Scrolls the given text on the screen"
   (let ((list-string (string->list str))
         (temp '()))
     (progn
@@ -27,7 +34,8 @@
     (loop 
       while t
       do (progn 
-           (shell "clear")
+           ;(format t "~C[h~C[2j" #\Escape #\Escape)
+           (format t "~C[2k~C[\r" #\Esc #\Esc)
            (push (pop list-string) temp)
            (nreverse list-string)
            (push (car temp) list-string)
@@ -35,3 +43,10 @@
            (princ list-string)
            (sleep 0.25)))))
 
+(shell "clear")
+
+;; this disables the cursor. But I don't know how to listen 
+;; for a SIGINT signal and handle that to restore the cursor back to its orginal state
+;(format t "~C[?25l" #\Esc)
+
+(scroll "hello world")
