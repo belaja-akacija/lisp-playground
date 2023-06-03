b5aa0d2383b029202550c891d0746eed6478c1ca
belaja-akacija <oselskyi@protonmail.com>
2023-06-03 02:57:54 -0700

    test commit

--- auto-wallpaper-changer/auto-wallpaper-changer.lisp
+++ auto-wallpaper-changer/auto-wallpaper-changer.lisp
@@ -8,7 +8,7 @@
 
 (defun random-from-range (start end)
   "generate a random number in a range"
-  (+ start (random (+ 1 (- end start)) (make-random-state t)))) ; look up later why you have to add (make-random-state) to make this work in compiled files
+  (+ start (random (+ 1 (- end start)) (make-random-state t))))
 
 (defun indexer (index list)
   "go to a particular index in list and pull it out"
@@ -20,5 +20,5 @@
 (defun wallpaper-changer ()
   "change to a random wallpaper using feh"
   (let* ((random-index (random-from-range 0 (1- (length *wallpapers*))))
-         (random-wallpaper (namestring (indexer random-index *wallpapers*))))
+         (random-wallpaper (namestring (indexer random-index *wallpapers*)))) ; lookup namestring for future reference. I keep forgetting what it does until I need it again.
     (uiop:run-program `("feh" "-Z" "--bg-center" ,random-wallpaper))))
