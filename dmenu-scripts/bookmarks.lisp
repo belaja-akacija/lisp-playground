91ac3834ffcf7c76bbdb8e34016e42fa4602d731
belaja-akacija <oselskyi@protonmail.com>
2023-06-22 07:59:41 -0700

    chore: generalised url file path

--- dmenu-scripts/bookmarks.lisp
+++ dmenu-scripts/bookmarks.lisp
@@ -11,13 +11,14 @@
 
 (defparameter *url-file-path* #P "~/Documents/.bmks/")
 (defparameter *url-file-name* "urls")
+(defparameter *url-full-path* (merge-pathnames *url-file-path* (pathname *url-file-name*)))
 (defparameter *first-arg* (string-downcase (car uiop:*command-line-arguments*)))
 
 (defun show-usage ()
   (format t "bkms: unix bookmark management that sucks less. Lisp edition!"))
 
 (defun append->file (url desc)
-  (with-open-file (output (merge-pathnames *url-file-path* (pathname *url-file-name*)) :direction :output
+  (with-open-file (output *url-full-path* :direction :output
                           :if-exists :append :if-does-not-exist :create)
     (format output "~A | ~A~%" url desc)))
 
@@ -34,18 +35,18 @@
 (defun bmks-display ()
   ;; This is currently seeming quite messy and bulky
   (let ((bmks-length  (string-trim `(#\NewLine) (uiop:run-program `("wc" "-l")
-                                                                  :input #P "/home/belajaakacija/Documents/.bmks/urls"
+                                                                  :input *url-full-path*
                                                                   :output :string)))
         (raw-entry "")
         (filtered-entry ""))
     (setq raw-entry (uiop:run-program `("dmenu" "-l", bmks-length)
-                                      :input #P "/home/belajaakacija/Documents/.bmks/urls"
+                                      :input *url-full-path*
                                       :output :string))
     (setq filtered-entry (cl-ppcre:scan-to-strings ".\+\(?=\\|\)" (string-trim '(#\NewLine) raw-entry)))
     (uiop:run-program `("firefox", filtered-entry))))
 
 (defun bmks-check ()
-  (cond ((null (probe-file (merge-pathnames *url-file-path* (pathname *url-file-name*))))
+  (cond ((null (probe-file *url-full-path*))
          (format t "Error: No bookmarks found to display. Try adding some!~%~%"))
         ((null (probe-directory *url-file-path*))
          (uiop:run-program `("mkdir" "-v", (directory-namestring *url-file-path*))))
