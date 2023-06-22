4a879d79ef7b40b36b56ee065cb946e5119d1f7c
belaja-akacija <oselskyi@protonmail.com>
2023-06-22 07:13:32 -0700

    feat: added ability to execute bookmarks in browser through dmenu. Also ported it over to sbcl

--- dmenu-scripts/bookmarks.lisp
+++ dmenu-scripts/bookmarks.lisp
@@ -1,10 +1,12 @@
-;#!/bin/clisp
-(load "~/quicklisp/setup.lisp")
+#!/home/belajaakacija/.local/bin/sbcl --script
 
-(defparameter *prefered-launcher* "dmenu")
+(load "~/quicklisp/setup.lisp")
+(ql:quickload "cl-ppcre")
+
 (defparameter *url-file-path* #P "~/Documents/.bmks/")
 (defparameter *url-file-name* "urls")
-(defparameter *first-arg* (string-downcase (car ext:*args*)))
+;(defparameter *first-arg* (string-downcase (car ext:*args*)))
+(defparameter *first-arg* (string-downcase (car uiop:*command-line-arguments*)))
 
 (defun show-usage ()
   (format t "bkms: unix bookmark management that sucks less. Lisp edition!"))
@@ -17,14 +19,29 @@
 (defun bmks-add ()
   ;(if (null uiop:*command-line-arguments*)
   (let ((desc ""))
-   (if (null (cadr ext:*args*))
+   ;(if (null (cadr ext:*args*))
+   (if (null (cadr uiop:*command-line-arguments*))
        (format t "Error: url must be provided.~%~%")
        (progn
          ;(format t "Description: ")
          (setq desc (uiop:run-program `("dmenu" "-l" "6" "-p" "Description: ") :output :string))
          (print desc)
-         (append->file (cadr ext:*args*) (string-trim '(#\NewLine) desc))
+         ;(append->file (cadr ext:*args*) (string-trim '(#\NewLine) desc))
+         (append->file (cadr uiop:*command-line-arguments*) (string-trim '(#\NewLine) desc))
          ))))
+
+(defun bmks-display ()
+  ;; This is currently seeming quite messy and bulky
+  (let ((bmks-length  (string-trim `(#\NewLine) (uiop:run-program `("wc" "-l")
+                                               :input #P "/home/belajaakacija/Documents/.bmks/urls"
+                                               :output :string)))
+        (raw-entry "")
+        (filtered-entry ""))
+(setq raw-entry (uiop:run-program `("dmenu" "-l", bmks-length)
+                  :input #P "/home/belajaakacija/Documents/.bmks/urls"
+                  :output :string))
+    (setq filtered-entry (cl-ppcre:scan-to-strings ".\+\(?=\\|\)" (string-trim '(#\NewLine) raw-entry)))
+    (uiop:run-program `("firefox", filtered-entry))))
 
 (defun bmks-check ()
   (cond ((null (probe-file (merge-pathnames *url-file-path* (pathname *url-file-name*))))
@@ -37,6 +54,8 @@
   (cond ((string-equal "add" *first-arg*)
          (bmks-add))
         ((not (null (find *first-arg* '("help" "h") :test #'string-equal)))
-         (show-usage))))
+         (show-usage))
+        ((string-equal "nil" *first-arg*)
+         (bmks-display))))
 ;; finish the rest of the cases
 (main)
